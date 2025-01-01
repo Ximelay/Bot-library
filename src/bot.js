7d99d79d358101e231ecd7cea5877875373ec359
Ximelay <iliaimax64@gmail.com>
2025-01-01 18:02:15 +0800

    Add /search

--- src/bot.js
+++ src/bot.js
@@ -1,9 +1,14 @@
 require('dotenv').config()
 const TelegramBot = require('node-telegram-bot-api')
 const startCommand = require('./commands/start')
+const searchCommand = require('./commands/search')
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, {polling: true});
 
 bot.onText(/\/start/, (msg) => startCommand(bot, msg));
 
+bot.onText(/\/search(.*)/, (msg, match) => {
+    console.log(`[LOG]: Команда /search вызвана пользователем ${msg.chat.id}`);
+    searchCommand(bot, msg, match);
+});
 console.log('Бот запущен')
